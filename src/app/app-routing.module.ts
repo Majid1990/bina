9f44445dfd540cac6a416edc297d509cb0124667
Majid <majid.jalilvand2000@gmail.com>
2022-07-17 15:51:24 +0100

    Added reports and it's module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'add-monitor', loadChildren: () => import('./add-monitor/add-monitor/add-monitor.module').then(x => x.AddMonitorModule) },
+  { path: 'reports', loadChildren: () => import('./reports/reports/reports.module').then(x => x.ReportsModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutComponent } from './layout/layout.component';
+//import { AddMonitorComponent } from './add-monitor/add-monitor.component';
+import { HomeComponent } from './home/home.component';
+// import { ReportsComponent } from './reports/reports.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LayoutComponent
+    LayoutComponent,
+    HomeComponent,
+
   ],
   imports: [
     BrowserModule,
