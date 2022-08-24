813a0c6bb6f66574fd27189e1f1cd1691cf1555f
Majid <majid.jalilvand2000@gmail.com>
2022-08-24 14:56:49 +0200

    Added directives

--- src/app/ip.directive.ts
+++ src/app/ip.directive.ts
@@ -8,7 +8,7 @@ export class IpDirective implements OnInit {
 
   @HostListener('input') keyup(eventData: any) {
     // const ip = this.ipService.formsValue;
-    const ip = this.el.nativeElement
+    const ip = this.el.nativeElement.value;
     var lastDot = ip.lastIndexOf('.');
 
     if (ip.length - lastDot === 4) {
@@ -16,21 +16,12 @@ export class IpDirective implements OnInit {
       const firstPart = ip.substring(0, ip.length - 1);
       const lastPart = ip.substring(ip.length - 1, ip.length);
       const final = firstPart + '.' + lastPart;
-      // this.switchForm?.get('switchIP')?.setValue(final);
-      // var finalResult = this.el.nativeElement.setValue(final)
-      // this.ipService.finalForm.setValue(finalResult)
-      this.el.nativeElement
+      this.el.nativeElement.value = final
     }
   }
 
   constructor(private el: ElementRef, private ipService: IpService) { }
   ngOnInit() {
-
-
-
   }
-
-
-
 
 }
